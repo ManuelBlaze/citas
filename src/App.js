654f2f488d9bb737f0dc8370e5f3ac515b60f1b3
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-06-29 00:21:52 -0500

    Administrar citas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Fragment, useState} from 'react';
 import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 
 function App() {
 
@@ -10,6 +11,15 @@ function App() {
   const crearCita = cita => {
     guardarCitas([...citas, cita]);
   }
+
+  //Función que elimina una cita por su ID
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    guardarCitas(nuevasCitas);
+  }
+
+  //Mensaje condicional
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus Citas';
 
   return (
     <Fragment>
@@ -23,7 +33,14 @@ function App() {
             />
           </div>
           <div className="one-half column">
-            2
+            <h2>{titulo}</h2>
+            {citas.map(cita => (
+              <Cita 
+                key={cita.id}
+                cita={cita}
+                eliminarCita={eliminarCita}
+              />
+            ))}
           </div>
         </div>
       </div>
